08262b3d921405ff3e13c3ff6625937bcacbda4c
vuongminh042 <vuongchiminh320@gmail.com>
2024-07-25 10:49:25 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { Route, Routes } from "react-router-dom"
+import CustomLayout from "./Layout"
+import ProductList from "./ProductList"
+import ProductAdd from "./ProductAdd"
+import ProductEdit from "./ProductEdit"
+import Signup from "./SignUp"
+import Signin from "./SignIn"
 
 function App() {
   return (
-    <>
-
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<CustomLayout />}>
+          <Route path="products" element={<ProductList />} />
+          <Route path="products/add" element={<ProductAdd />} />
+          <Route path="products/:id/edit" element={<ProductEdit />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="signin" element={<Signin />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
