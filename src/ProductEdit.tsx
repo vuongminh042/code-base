deae5cb0ce72a05fd427d68d1dd9a26a78f85226
vuongminh042 <vuongchiminh320@gmail.com>
2024-07-25 10:44:50 +0700

    update

---
+++ src/ProductEdit.tsx
@@ -0,0 +1,96 @@
+import { joiResolver } from "@hookform/resolvers/joi";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Joi from "joi"
+import { useForm } from "react-hook-form";
+import { useNavigate, useParams } from "react-router-dom";
+
+
+type FormData = {
+    id?: number,
+    name: string,
+    price: number,
+    image: string,
+    description: string
+}
+
+const productSchema = Joi.object({
+    name: Joi.string().required().min(3),
+    price: Joi.number().required().positive().messages({
+        'number.base': `"price" phải là một số`,
+    }),
+    image: Joi.string(),
+    description: Joi.string()
+});
+
+
+const ProductEdit = () => {
+    const { id } = useParams()
+    const navigate = useNavigate()
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>({
+        resolver: joiResolver(productSchema),
+        defaultValues: {
+            name: "",
+            price: 0,
+            image: "",
+            description: ""
+        }
+    })
+
+    useQuery({
+        queryKey: ['products', id],
+        queryFn: async () => {
+            const response = await axios.get(`http://localhost:3000/products/${id}`)
+            reset(response.data)
+            return response.data
+        }
+    })
+
+    const mutation = useMutation({
+        mutationFn: async (formData: FormData) => {
+            const response = await axios.put(`http://localhost:3000/products/${formData.id}`, formData)
+            return response.data
+        },
+        onSuccess: () => {
+            alert('Update Successful')
+            navigate('/products')
+        },
+        onError: (error) => {
+            alert('Update Failed' + error.message)
+        }
+    })
+
+    const onSubmit = (formData: FormData) => {
+        mutation.mutate(formData);
+    }
+
+    return (
+        <div>
+            <h1>Product Edit</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Product Name</label>
+                    <input type="text" className="form-control" id="name" {...register('name', { required: true, minLength: 3 })} />
+                    {errors?.name && <div id="name" className="form-text text-danger">{errors?.name?.message}</div>}
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="price" className="form-label">Product Price</label>
+                    <input type="number" className="form-control" id="price" {...register('price', { required: true })} />
+                    {errors?.price && <div id="price" className="form-text text-danger">{errors?.price?.message}</div>}
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="image" className="form-label">Product Image</label>
+                    <input type="text" className="form-control" id="image" {...register('image')} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="description" className="form-label">Product Description</label>
+                    <textarea id="description" className="form-control" {...register('description')}></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+
+        </div>
+    )
+}
+
+export default ProductEdit
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { StyleProvider } from '@ant-design/cssinjs'
 
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <StyleProvider layer>
+        <App />
+      </StyleProvider>
+    </BrowserRouter>
+  </QueryClientProvider>,
 )
