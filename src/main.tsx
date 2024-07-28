fb535f1d7eea79fea813f984b1e67efc048c3476
vuongminh042 <vuongchiminh320@gmail.com>
2024-07-28 23:42:34 +0700

    update code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { StyleProvider } from '@ant-design/cssinjs'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <StyleProvider layer>
+        <App />
+      </StyleProvider>
+    </BrowserRouter>
+  </QueryClientProvider>,
 )
