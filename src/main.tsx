17bece695af8e97795043386f671784b032813fd
vuongminh042 <vuongchiminh320@gmail.com>
2024-07-29 21:21:14 +0700

    reset code

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
-import { StyleProvider } from '@ant-design/cssinjs'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 
-const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <StyleProvider layer>
-        <App />
-      </StyleProvider>
-    </BrowserRouter>
-  </QueryClientProvider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
