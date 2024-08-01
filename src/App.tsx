1d1afbe47762f372e81f3a581aa6ef26c9f63815
vuongminh042 <vuongchiminh320@gmail.com>
2024-08-01 20:21:53 +0700

    reset code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import { Route, Routes } from "react-router-dom"
-import CustomLayout from "./Layout"
-import ProductList from "./ProductList"
-import ProductAdd from "./ProductAdd"
-import ProductEdit from "./ProductEdit"
-import SignUp from "./SignUp"
-import SignIn from "./SignIn"
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<CustomLayout />}>
-          <Route path="products" element={<ProductList />} />
-          <Route path="products/add" element={<ProductAdd />} />
-          <Route path="products/:id/edit" element={<ProductEdit />} />
-          <Route path="signup" element={<SignUp />} />
-          <Route path="signin" element={<SignIn />} />
-        </Route>
-      </Routes>
-    </div>
+    <>
+
+    </>
   )
 }
 
